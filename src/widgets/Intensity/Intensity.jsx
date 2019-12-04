9722f92dd550dd7e59d8980dbc03e975993df72b
sadiqshammu <58417316+sadiqshammu@users.noreply.github.com>
2019-12-04 10:38:48 +0530

    Intensity code update

--- src/widgets/Intensity/Intensity.jsx
+++ src/widgets/Intensity/Intensity.jsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Roundy from "roundy"
 import AppContext from "../../components/State"
 import { Legend } from "../Legend"
 import "./styles.css"
@@ -6,30 +7,22 @@ import "./styles.css"
 function Intensity(props) {
   const { className = "", ...rest } = props
   const getContext = React.useContext(AppContext)
-  const defVal = -90
-  const curVal = getContext.level
-  const minMaxVal = defVal + curVal + (curVal - 10)
-  const transformVal = minMaxVal > 90 ? 90 : minMaxVal < -90 ? -90 : minMaxVal
+
   return (
     <section {...rest} className={`intensity-wrapper ${className}`}>
       <Legend text="intensity" />
       <div className="intensity">
-        <div
-          className="line"
-          style={{ transform: `rotate(${transformVal}deg)` }}
-        >
-          <div class="small-circle" />
-        </div>
+        <Roundy
+          value={getContext.level}
+          allowClick
+          color="#AD6BFF"
+          arcSize={180}
+          min={0}
+          max={100}
+          rotationOffset={0}
+          strokeWidth={2}
+        />
         <span className="count">{getContext.level}</span>
-        <div className="circle">
-          <div
-            className="overlay"
-            style={{ width: `${curVal <= 30 ? curVal - 8 : curVal}%` }}
-          >
-            <div className="circle-bold" />
-          </div>
-          <div className="circle-thin" />
-        </div>
       </div>
     </section>
   )
